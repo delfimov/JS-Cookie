827cadd318913793017e2edfa08b3603712feb57
Groozly <elfimov@gmail.com>
2012-02-21 18:13:04 +0400

    create

---
+++ cookie.js
@@ -0,0 +1,57 @@
+/*
+ * cookie javascript library 
+ * https://github.com/Groozly/JS-Cookie
+ *
+ * Copyright (c) 2011 by Dmitry Elfimov
+ * Released under the MIT License.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Date: 2011-09-15
+ */
+
+var cookie = {
+
+	set: function(k, v, e, p, d, s) {
+		var c = k+'='+escape(v)+'; ';
+		if (e !== undefined) {
+			var t = new Date();
+			t.setTime(t.getTime()+(e*24*60*60*1000));
+			c+= 'expires='+t.toGMTString()+'; ';
+		}
+		c += (p === undefined) ? 'path=/;' : 'path='+p+'; ';
+		c += (d === undefined) ? '' : 'domain='+d+'; '
+		c += (s === true) ? 'secure; ' : '';
+		document.cookie = c;
+	},
+
+	get: function(k) {
+		var v = '';
+		var ke = k+ '=';
+		var c = document.cookie.split(';');
+		for (var i = 0, l = c.length; i < l; i++) {
+			while (c[i].charAt(0) === ' ') {
+				c[i] = c[i].substring(1);
+			}
+			if (c[i].indexOf(ke) === 0) {
+				v = c[i].substring(ke.length);
+			}
+		}
+		return unescape(v);
+	},
+
+	remove: function(k) {
+		cookie.set(k, '', -1);
+	},
+
+	isEnabled: function() {
+		var e = (navigator.cookieEnabled) ? true : false;
+		var t = '_test';
+		if (typeof navigator.cookieEnabled == "undefined" && !e) {
+			cookie.set(t, t);
+			var e = (cookie.get(t) === t) ? true : false;
+			cookie.remove(t);
+		}
+		return e;
+	}
+
+};
